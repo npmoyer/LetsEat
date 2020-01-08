3dd74a90015df9f1015601ee24fff570bb300c8f
Nick Moyer <moyer.n.p@gmail.com>
2020-01-07 22:22:46 -0600

    AndroidX Migration

--- app/src/main/java/com/labs/nipamo/letseat/DetailedMapActivity.java
+++ app/src/main/java/com/labs/nipamo/letseat/DetailedMapActivity.java
@@ -4,8 +4,8 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
--- app/src/main/java/com/labs/nipamo/letseat/MapsActivity.java
+++ app/src/main/java/com/labs/nipamo/letseat/MapsActivity.java
@@ -8,8 +8,8 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 import android.widget.Toast;
 
 import com.android.volley.Request;
